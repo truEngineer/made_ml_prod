24fa5a0af698202a05ee9f9ec425420ba74359fa
truEngineer <plokoon178@gmail.com>
2021-05-10 19:08:35 +0300

    transformer test

--- ml_project/tests/features/test_build_features.py
+++ ml_project/tests/features/test_build_features.py
@@ -1,11 +1,24 @@
+import pytest
 import pandas as pd
 from numpy.testing import assert_allclose
 
 from src.entities import FeatureParams
 from src.data import read_data
 from src.features.build_features import (
-    make_features, extract_target, build_transformer,
+    make_features, extract_target,
+    build_transformer, CustomScaler,
 )
+
+
+def test_custom_scaler(feature_params: FeatureParams, data_path: str):
+    data = read_data(data_path)
+    num_cols = feature_params.numerical_features
+    data = data[num_cols]
+    transformer = CustomScaler()
+    transformer.fit(data)
+    transformed_data = transformer.transform(data).to_numpy()
+    assert transformed_data.mean() == pytest.approx(0, 0.1)
+    assert transformed_data.std() == pytest.approx(1, 0.1)
 
 
 def test_make_features(feature_params: FeatureParams, data_path: str):
