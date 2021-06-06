3171fd478ce197128fbe425d8836252d80e1d292
truEngineer <plokoon178@gmail.com>
2021-06-06 15:33:22 +0300

    logreg

--- airflow_ml_dags/images/airflow-train/train.py
+++ airflow_ml_dags/images/airflow-train/train.py
@@ -7,8 +7,7 @@ import click
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV  # StratifiedKFold
-# from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 @click.command("train")
@@ -31,13 +30,6 @@ def train(input_dir: str, output_dir: str):
     y_train = train_data["target"]
 
     logger.info("Train model.")
-    #model = RandomForestClassifier(random_state=42)
-    #param_grid = {"n_estimators": [10, 30, 50], "max_depth": [1, 5, 10]}
-    #cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
-    #grid = GridSearchCV(model, param_grid, cv=cv, n_jobs=-1)
-    #grid.fit(X_train, y_train)
-    #logger.info(f"Best accuracy: {grid.best_score_:.2f}")
-    #logger.info(f"Best params: {grid.best_params_}")
     param_grid = {"C": np.logspace(-3, 3, 10)}
     logreg = LogisticRegression(multi_class="multinomial")  # cross-entropy loss
     logreg_cv = GridSearchCV(logreg, param_grid, cv=5)  # stratified
@@ -49,10 +41,10 @@ def train(input_dir: str, output_dir: str):
     os.makedirs(output_dir, exist_ok=True)
 
     with open(os.path.join(output_dir, "model.pkl"), "wb") as f:
-        pickle.dump(logreg_cv.best_estimator_, f)  # grid
+        pickle.dump(logreg_cv.best_estimator_, f)
 
     with open(os.path.join(output_dir, "metrics.json"), "w") as f:
-        json.dump({"accuracy": logreg_cv.best_score_}, f)  # grid
+        json.dump({"accuracy": logreg_cv.best_score_}, f)
 
     logger.info("Model ready.")
 
